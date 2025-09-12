2721971f056d51be102bf8564fc3c729bedf7b53
ahnufmir <mirahnaf55@gmail.com>
2025-09-12 17:35:34 +0500

    Practse..

--- opencv-1.py
+++ opencv-1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 image = cv2.imread("germany.png")
@@ -45,16 +46,44 @@ print("---- Welcome to OpenCV --------")
 ## IMAGE MODIFICATION
 # Image Crop
 
-cropped_image = image[100:200,50:150]
-cv2.imshow("Image", cropped_image)
+# cropped_image = image[100:200,50:150]
+# cv2.imshow("Image", cropped_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# # Image Resize
+# resized_img = cv2.resize(image,(925,925))
+# cv2.imshow("Image", resized_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#Greycale Image
+# print(image.shape)
+# grey_scale_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# print(grey_scale_img.shape)
+
+#Playing with the channels of the image
+# imgBlue = image[:,:,0]
+# imgGreen = image[:,:,1]
+# imgRed = image[:,:,2]
+# new_img = np.hstack((imgBlue,imgGreen,imgRed))
+# cv2.imshow("heyyyyyyyyyyy",new_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#Flipping an image
+# flipped_img = cv2.flip(image, 0)
+# cv2.imshow("heyyyyyyyyyyy",flipped_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# Drawing Shapes
+
+img = np.zeros((500,500,3))
+
+cv2.rectangle(img, pt1=(100,100), pt2=(200,250), color=(0,0,255), thickness=100)
+cv2.line(img, pt1=(0,0), pt2=(500,500), color=(255,0,0), thickness=100)
+cv2.circle(img, center=(250,300), radius=50, color=(0,255,0), thickness=100 )
+cv2.imshow("window", img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# Image Resize
-resized_img = cv2.resize(image,(100,100))
-cv2.imshow("Image", resized_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-df_cat = df.select_dtypes(np.object_)
-# df_num = df.select_dtypes(np.number)
-# df_float = df.select_dtypes(np.float64)
-
-# print("Object Columns: \n",df_cat)
-# print("Numeric Columns: \n",df_num)
-# print("Float Columns: \n",df_float)
+cv2
